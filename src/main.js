e5bea50d314c67b74c252b7f02a38566a8a070ae
Marc-Olivier Laux <marc-olivier@bebe-au-naturel.com>
2018-03-11 11:12:38 +0100

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,47 @@
+var Munin = require('./munin.js');
+
+var dateformat = require('dateformat');
+var strtotime = require('strtotime');
+
+let args = process.argv.slice(2);
+let show = false;
+if (args[0] == 'show') {
+    args = args.slice(1);
+    show = true;
+}
+
+let [domain, host, probe, target, from, to, fc] = args;
+
+if (from != undefined) {
+    from = strtotime(from);
+} else {
+    from = strtotime('1 hour ago');
+}
+
+if (to != undefined) {
+    to = strtotime(to);
+} else {
+    to = new Date();
+}
+
+console.log('From : ', dateformat(from, "dd/mm/yyyy HH:MM:ss"));
+console.log('To   : ', dateformat(to, "dd/mm/yyyy HH:MM:ss"));
+
+let munin = new Munin('/home/marco/src/munin-db-test');
+
+munin.load().then( data => {
+        if (show) {
+            console.log(munin.describe(domain, host, probe, target));
+        } else {
+            munin.query(domain, host, probe, target, from, to, fc).then( data => {
+                if (target != undefined) {
+                    data = data.map( record => {
+                        record.time = dateformat(new Date(record.time * 1000), "dd/mm/yyyy HH:MM:ss");
+                        return record;
+                    } );
+                }
+                console.log(data);
+            })
+        }
+    }
+);
