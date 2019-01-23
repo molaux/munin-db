f653e91f2fd1286af2e912587ec0f26a1795c2f9
Marc-Olivier Laux <marco@matlaux.net>
2019-01-23 16:48:35 +0100

    Upgrade

--- src/main.js
+++ src/main.js
@@ -1,47 +1,49 @@
-var Munin = require('./munin.js')
+var Munin = require('../lib/munin.js');
 
-var dateformat = require('dateformat')
-var strtotime = require('strtotime')
+var dateformat = require('dateformat');
+var strtotime = require('locutus/php/datetime/strtotime');
 
-let args = process.argv.slice(2)
-let show = false
-if (args[0] === 'show') {
-  args = args.slice(1)
-  show = true
+let args = process.argv.slice(2);
+let show = false;
+if (args[0] == 'show') {
+    args = args.slice(1);
+    show = true;
 }
 
-let [domain, host, probe, target, from, to, fc] = args
+let [domain, host, probe, target, from, to, fc] = args;
 
-if (from !== undefined) {
-  from = strtotime(from)
+if (from != undefined) {
+    console.log(from);
+    from = new Date(strtotime(from) * 1000);
+    console.log(from);
 } else {
-  from = strtotime('1 hour ago')
+    from = new Date(strtotime('1 hour ago') * 1000);
 }
 
-if (to !== undefined) {
-  to = strtotime(to)
+if (to != undefined) {
+    to = new Date(strtotime(to) * 1000);
 } else {
-  to = new Date()
+    to = new Date();
 }
 
-console.log('From : ', dateformat(from, 'dd/mm/yyyy HH:MM:ss'))
-console.log('To   : ', dateformat(to, 'dd/mm/yyyy HH:MM:ss'))
+console.log('From : ', dateformat(from, "dd/mm/yyyy HH:MM:ss"));
+console.log('To   : ', dateformat(to, "dd/mm/yyyy HH:MM:ss"));
 
-let munin = new Munin('/home/marco/src/munin-db-test')
+let munin = new Munin('/var/lib/munin');
 
-munin.load().then(data => {
-  if (show) {
-    console.log(munin.describe(domain, host, probe, target))
-  } else {
-    munin.query(domain, host, probe, target, from, to, fc).then(data => {
-      if (target !== undefined) {
-        data = data.map(record => {
-          record.time = dateformat(new Date(record.time * 1000), 'dd/mm/yyyy HH:MM:ss')
-          return record
-        })
-      }
-      console.log(data)
-    })
-  }
-}
-)
+munin.load().then( data => {
+        if (show) {
+            console.log(munin.describe(domain, host, probe, target));
+        } else {
+            munin.query(domain, host, probe, target, from, to, null, fc).then( data => {
+                if (target != undefined) {
+                    data = data.map( record => {
+                        record.time = dateformat(new Date(record.time * 1000), "dd/mm/yyyy HH:MM:ss");
+                        return record;
+                    } );
+                }
+                console.log(data);
+            })
+        }
+    }
+);
