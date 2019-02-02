88757979c66e9e9a9f768fef6c85be36fa86b221
Marc-Olivier Laux <marc-olivier@bebe-au-naturel.com>
2019-02-02 20:19:54 +0100

    Add limits feature

--- src/main.js
+++ src/main.js
@@ -2,15 +2,12 @@ var Munin = require('../lib/munin.js');
 
 var dateformat = require('dateformat');
 var strtotime = require('locutus/php/datetime/strtotime');
+const util = require('util')
 
 let args = process.argv.slice(2);
-let show = false;
-if (args[0] == 'show') {
-    args = args.slice(1);
-    show = true;
-}
 
-let [domain, host, probe, target, from, to, fc] = args;
+
+let [action, domain, host, probe, target, from, to, fc] = args;
 
 if (from != undefined) {
     console.log(from);
@@ -26,24 +23,39 @@ if (to != undefined) {
     to = new Date();
 }
 
-console.log('From : ', dateformat(from, "dd/mm/yyyy HH:MM:ss"));
-console.log('To   : ', dateformat(to, "dd/mm/yyyy HH:MM:ss"));
 
 let munin = new Munin('/var/lib/munin');
 
-munin.load().then( data => {
-        if (show) {
+switch (action) {
+  case 'query' :
+  case 'describe' :
+    munin.load().then( data => {
+
+        switch (action) {
+          case 'describe':
             console.log(munin.describe(domain, host, probe, target));
-        } else {
+            break;
+
+          case 'query':
+            console.log('From : ', dateformat(from, "dd/mm/yyyy HH:MM:ss"));
+            console.log('To   : ', dateformat(to, "dd/mm/yyyy HH:MM:ss"));
+
             munin.query(domain, host, probe, target, from, to, null, fc).then( data => {
-                if (target != undefined) {
-                    data = data.map( record => {
-                        record.time = dateformat(new Date(record.time * 1000), "dd/mm/yyyy HH:MM:ss");
-                        return record;
-                    } );
-                }
-                console.log(data);
-            })
+              if (target != undefined) {
+                data = data.map( record => {
+                  record.time = dateformat(new Date(record.time * 1000), "dd/mm/yyyy HH:MM:ss");
+                  return record;
+                });
+              }
+              console.log(data);
+            });
+            break;
         }
-    }
-);
+
+      });
+
+    break;
+  case 'limits' :
+    munin.limits(domain, host, probe, target).then(data => console.log(util.inspect(data, false, null, true)));
+    break;
+}
